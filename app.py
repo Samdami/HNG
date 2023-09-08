75eaf882f5a19dc78873ea9a71d43e4c9c364ab2
Samuel Damilola Akinmarin <111143232+Samdami@users.noreply.github.com>
2023-09-08 15:50:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-import datetime
+from datetime import datetime, timedelta
+import pytz
 
 app = Flask(__name__)
 
@@ -8,8 +9,9 @@ def api():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
     
-    current_day = datetime.datetime.utcnow().strftime('%A')
-    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    nigeria_timezone = pytz.timezone('Africa/Lagos')
+    nigeria_time = datetime.now(nigeria_timezone).strftime('%Y-%m-%dT%H:%M:%SZ')
+    current_day = datetime.now(nigeria_timezone).strftime('%A')
     
     # Assuming you're running this script from a GitHub repo
     github_file_url = "https://github.com/Samdami/HNG/blob/main/Stage1.py"
@@ -18,7 +20,7 @@ def api():
     response = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "utc_time": utc_time,
+        "nigeria_time": nigeria_time,
         "track": track,
         "github_file_url": github_file_url,
         "github_repo_url": github_repo_url,
